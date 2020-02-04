30052f469b79b03876725567c7fdea726ba96745
dkparmar <dk.parmar2000@gmail.com>
2020-02-04 16:29:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          we did it why not we can dod  sda value
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         google the React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
